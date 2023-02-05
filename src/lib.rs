b3e500fe59a31581eb6ec7888b4f919c8e780303
Sparrow0hawk <alexjcoleman@gmail.com>
2023-02-05 13:37:07 +0000

    change run to take an address string

--- src/lib.rs
+++ src/lib.rs
@@ -10,13 +10,13 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(address: &str) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
     })
-    .bind("127.0.0.1:8000")?
+    .bind(address)?
     .run();
     Ok(server)
 }
