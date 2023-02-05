6a9447c2845c990401fb44bac614a4a61a742d98
Sparrow0hawk <alexjcoleman@gmail.com>
2023-02-05 13:23:42 +0000

    refactor app to run as background task

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use actix_web::dev::Server;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
@@ -9,13 +10,13 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| {
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
     })
     .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    .run();
+    Ok(server)
 }
