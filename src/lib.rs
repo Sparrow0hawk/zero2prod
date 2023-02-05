b468240123919f6475e42f2fc41f07c66dfb198d
Sparrow0hawk <alexjcoleman@gmail.com>
2023-02-05 13:45:04 +0000

    use tcplistener to allow flexibility of port use for testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -10,13 +11,13 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
-pub fn run(address: &str) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
     })
-    .bind(address)?
+    .listen(listener)?
     .run();
     Ok(server)
 }
