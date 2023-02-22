fdb16a5164c6185e53762813b16002dcfffb65fa
Sparrow0hawk <alexjcoleman@gmail.com>
2023-02-22 12:59:06 +0000

    add basic form data and subscribe function/endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,22 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
