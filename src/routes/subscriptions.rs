cb9d22f393b61d5ea0a0c33976364ec64f63dd05
Sparrow0hawk <alexjcoleman@gmail.com>
2023-07-02 20:32:39 +0100

    migrate log calls to tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,8 +11,19 @@ pub struct FormData {
 
 pub async fn subscribe(form: web::Form<FormData>, connection: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    log::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber details in the database",
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber details in the database",
+        // maps to request_id = %request_id
+        %request_id,
+        // % means Display format
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+    // not a good idea
+    let _request_span_guard = request_span.enter();
+
+    tracing::info!(
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
         request_id,
         form.email,
         form.name
@@ -31,11 +42,11 @@ pub async fn subscribe(form: web::Form<FormData>, connection: web::Data<PgPool>)
     .await
     {
         Ok(_) => {
-            log::info!("New subscriber details have been saved");
+            tracing::info!("New subscriber details have been saved");
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!("Failed to execute query: {:?}", e);
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
